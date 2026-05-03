e27820cb6da26cf6101c3b5b424799b2c845667a
aymenlemmochi-debug <aymenlemmochi@gmail.com>
2026-05-03 17:35:16 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,42 @@
+import requests
+
+def get_signal(rsi, ema20, ema50, macd, price):
+    if 45 < rsi < 55:
+        return "WAIT ⏳ سوق ضعيف"
+
+    buy_score = 0
+    sell_score = 0
+
+    if rsi > 55:
+        buy_score += 1
+    if ema20 > ema50:
+        buy_score += 1
+    if macd > 0:
+        buy_score += 1
+    if price > ema20:
+        buy_score += 1
+
+    if rsi < 45:
+        sell_score += 1
+    if ema20 < ema50:
+        sell_score += 1
+    if macd < 0:
+        sell_score += 1
+    if price < ema20:
+        sell_score += 1
+
+    if buy_score >= 4:
+        return "BUY 🔥 قوي"
+    elif buy_score == 3:
+        return "BUY ⚡ متوسط"
+
+    if sell_score >= 4:
+        return "SELL 🔥 قوي"
+    elif sell_score == 3:
+        return "SELL ⚡ متوسط"
+
+    return "WAIT ⏳ لا تدخل"
+
+
+# مثال تجربة
+print(get_signal(60, 100, 90, 0.5, 105))
